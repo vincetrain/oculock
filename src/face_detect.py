b11ea61beb77725fef6a0d65413044612b32d647
Vincent <tran.vincent@outlook.com>
2021-12-27 03:55:13 -0500

    Removed main.py, updated face_detect.py

--- src/face_detect.py
+++ src/face_detect.py
@@ -2,41 +2,53 @@
 import cv2
 import numpy as np
 
-cam = cv2.VideoCapture(0) # setup camera
 
-# creates cascade from provided cascade file
-cascPath = "haarcascade_eye.xml"
-eyeCascade = cv2.CascadeClassifier(cascPath)
+def scanEyes():
+    cam = cv2.VideoCapture(0) # setup camera
+    w = cam.get(cv2.CAP_PROP_FRAME_WIDTH)
+    h = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
+    # creates cascade from provided cascade file
+    cascPath = "haarcascade_eye.xml"
+    eyeCascade = cv2.CascadeClassifier(cascPath)
 
-ret, frame = cam.read()
-# reads camera for available faces
-while True:
-    ret, frame = cam.read()
-    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-    # frame = cv2.flip(frame, 1)
-    if not ret:
-        print("failed grabbing frame")
-        break
-    
-    key = cv2.waitKey(1)
-    
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    eyes = eyeCascade.detectMultiScale(
-        gray,
-        scaleFactor = 1.1,
-        minNeighbors = 40,
-        minSize = (30, 30),
-        flags = cv2.CASCADE_SCALE_IMAGE
-    )
-    
-    for (x, y, w, h) in eyes:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    cv2.imshow("test", frame)
+    # reads camera for available faces
+    while True:
+        ret, frame = cam.read()
+        frame = cv2.resize(frame, (1280, 720))   # resizes frame to 
+        # frame = cv2.flip(frame, 1)
+        if not ret:
+            print("failed grabbing frame")
+            break
         
-    if (key == ord("q")):
-        break
+        key = cv2.waitKey(1)
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        eyes = eyeCascade.detectMultiScale(
+            gray,
+            scaleFactor = 1.1,
+            minNeighbors = 40,
+            minSize = (int(w/7), int(h/7)),
+            flags = cv2.CASCADE_SCALE_IMAGE
+        )
+        
+        for (x, y, w, h) in eyes:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.imshow("test", frame)
+        
+        if (len(eyes) > 0):
+            cv2.waitKey(0)
+            if (key == 27):
+                break
 
-cam.release()
-cv2.destroyAllWindows()
+    cam.release()
+    cv2.destroyAllWindows()
+    
+    
+def calculateWinDimension(x):
+    while x > 512:
+        x-=5
+    return int(x),
+
+scanEyes()
--- src/main.py
+++
@@ -1,2 +0,0 @@
-import face_detect
-
